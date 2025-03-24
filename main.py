427f0f9f79ea9c1778f2c939b48716a645fb4d07
user_dam <damx@damx>
2025-03-24 16:00:20 +0100

    Modificar saludo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Mi primera aplicación con Flet"
-    page.add(ft.Text("¡Hola, Flet!"))
+    page.add(ft.Text("¡Hola, gente de informatica !!"))
 
 if __name__ == '__main__':
      ft.app(target=main,view=ft.WEB_BROWSER,port=8080)
